fe111a0e2f8c59fc8871edd8caf58c83b2f66bc2
MeghlaB <158607848+MeghlaB@users.noreply.github.com>
2024-02-03 00:29:26 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function login() {
+    // Add your login logic here
+    for (let i=0;i<=10;i++){
+    alert('Login button clicked!');
+    }
+  }
+  
+  function forgotPassword() {
+    // Add your forgot password logic here
+    alert('Forgot Password link clicked!');
+  }
+  
+  function createAccount() {
+    // Add your create account logic here
+    alert('Create a new account link clicked!');
+  }
+  
